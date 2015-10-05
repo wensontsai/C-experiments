280817c37892bfc23f68dfbe88e3bd92ac48e43e
Wenson Tsai <wensontsai@gmail.com>
2015-10-04 23:50:29 -0400

    echo program

--- fahrenheit_forloop.c
+++ fahrenheit_forloop.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/* symbolic constants - NOT VARIABLES */
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
+
 /* print Fahrenheit-Celsius table */
 int main()
 {
   int fahr;
 
-  for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for(fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
 }

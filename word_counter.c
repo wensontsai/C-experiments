3ce4cbaadfab5f81b6f1c140f019a7ca27b65071
Wenson Tsai <wensontsai@gmail.com>
2015-10-10 00:00:22 -0400

    fixes on wordcounter

--- word_counter.c
+++ word_counter.c
@@ -12,16 +12,13 @@ int main()
   nl = nw = nc = 0;
   while ( (c = getchar()) != EOF ){
     ++nc;
-    if (c == '\n'){
+    if (c == '\n')
         ++nl;
-    }
-    if (c == ' ' || c == '\n' || c == '\t'){
-        state == OUT;
-    }
-    else if (state == OUT){
+    if (c == ' ' || c == '\n' || c == '\t')
+        state = OUT;
+    else if (state == OUT)
       state = IN;
       ++nw;
-    }
   }
   printf("%d %d %d\n", nl, nw, nc);
 
